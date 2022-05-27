8b2467d415ba2448e63ee0427b287a9b050e4f2d
sumit2001 <sgsumitgupta17@gmail.com>
2022-05-27 12:26:34 +0530

    make it better

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://sumit2001:Engineer@cluster0.t08d1.mongodb.net/test?authSource=admin&replicaSet=atlas-sr8hhq-shard-0&readPreference=primary&ssl=true"
+require('dotenv').config();
+const mongoURI = process.env.ATLAS_URI
 
+console.log(mongoURI);
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully");
